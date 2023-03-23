8e981de50a39e79986f5ad62fb09738259d70cd6
naoki_kubota <naoki_kubota@jtekt.co.jp>
2023-03-23 10:56:35 +0900

    add fk

--- src/pytransform/chain.py
+++ src/pytransform/chain.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import numpy as np
+
 from .joint import BaseJoint
 from .tf import Transform
 
@@ -13,3 +15,19 @@ class Chain():
                  joints: list[BaseJoint]) -> None:
         self.links = links
         self.joints = joints
+
+    @property
+    def position(self):
+        return [j.position for j in self.joints]
+
+    def fk(self, positions: list):
+        for j, p in zip(self.joints, positions):
+            j.drive_to(p)
+
+    def ik_solve(self,
+                 end_effector_tf: Transform,
+                 target_position: np.ndarray):
+
+        def loss_position_f(x): return x
+
+        pass

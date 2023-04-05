b19e37d84df24c308bda192bbf08994424c422a7
naoki_kubota <naoki_kubota@jtekt.co.jp>
2023-04-05 14:48:15 +0900

    add ik test

--- tests/test_chain.py
+++ tests/test_chain.py
@@ -26,3 +26,28 @@ def test_read_urdf(filename: str, distal_name: str):
 
     end_effector = robot.get_link(distal_name)
     assert end_effector is not None
+
+
+test_ik_items = {
+    'mycobot': ('./files/mycobot_style.urdf', 'hand'),
+}
+
+
+@pytest.mark.parametrize(
+    "filename,distal_name",
+    list(test_ik_items.values()),
+    ids=list(test_ik_items.keys()))
+def test_ik(filename: str, distal_name: str):
+
+    filepath = os.path.join(
+        os.path.dirname(__file__),
+        filename
+    )
+    robot = urdf.chain_from_urdf(filepath)
+    end_effector = robot.get_link(distal_name)
+    assert end_effector is not None
+    print(f'end effector: {end_effector.name}')
+    l = robot.ik_solve(
+        [end_effector],
+        [np.array([0.1, 0.1, 0.1])]
+    )

8c45455b15038612a46c67fb122446abe5b828d3
naoki_kubota <naoki_kubota@jtekt.co.jp>
2023-04-28 11:58:26 +0900

    add camera projection func

--- src/pytransform/camera.py
+++ src/pytransform/camera.py
@@ -8,3 +8,11 @@ def camera_intrinsic_matrix(fx: float, fy: float, cx: float, cy: float):
         [0, fy, cy],
         [0, 0, 1]
     ])
+
+
+def project_to_camera_plane(
+        position: np.ndarray,
+        camera_matrix: np.ndarray
+) -> np.ndarray:
+    pc = (camera_matrix@position.reshape((3, 1))).ravel()
+    return pc/position[2]

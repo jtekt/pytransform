a729ebc06d7d765a850c2732e38fab1cabd954ec
naoki_kubota <naoki_kubota@jtekt.co.jp>
2023-06-01 14:56:46 +0900

    update ik

--- examples/ik_4limbs.py
+++ examples/ik_4limbs.py
@@ -16,45 +16,44 @@ from pytransform.joint import Limitation, RevoluteJoint
 from pytransform.tf import Transform
 
 
-def manipulator(filename: str,
-                target_positions: list[np.ndarray],
-                is_save: bool = False,
-                method='BFGS'):
-
+def manipulator(
+    filename: str,
+    target_positions: list[np.ndarray],
+    is_save: bool = False,
+    method="BFGS",
+):
     robot = urdf.chain_from_urdf(filename)
     mi, ma = robot.bbox()
-    size = (ma-mi)
+    size = ma - mi
 
     # visualize
-    fig = plt.figure(figsize=(800/72, 800/72))
-    ax: plt.Axes = fig.add_subplot(projection='3d')
+    fig = plt.figure(figsize=(800 / 72, 800 / 72))
+    ax: plt.Axes = fig.add_subplot(projection="3d")
     # pytf.plot.chain(robot, ax=ax, scale=size.mean()*0.1, show_label=False.as_integer_ratio,
     #                 cmap=pytf.plot.coordinate_cmap('Pastel1'))
 
-    print(f'robot name: {robot.name}')
-    print(f'#links: {len(robot.links)}')
-    print(f'#joints: {len(robot.joints)}')
+    print(f"robot name: {robot.name}")
+    print(f"#links: {len(robot.links)}")
+    print(f"#joints: {len(robot.joints)}")
 
-    hand_l = robot.get_link('left_hand_default')
-    hand_r = robot.get_link('right_hand_default')
-    foot_l = robot.get_link('left_foot_default')
-    foot_r = robot.get_link('right_foot_default')
+    hand_l = robot.get_link("left_hand_default")
+    hand_r = robot.get_link("right_hand_default")
+    foot_l = robot.get_link("left_foot_default")
+    foot_r = robot.get_link("right_foot_default")
 
     for t in [hand_l, hand_r, foot_l, foot_r]:
         if t is None:
-            print(f'target tf is not found')
+            print(f"target tf is not found")
             print(robot.tree())
             return
 
     bt = time.time()
     ik_result = robot.ik_solve(
-        [hand_l, hand_r, foot_l, foot_r],
-        target_positions,
-        robot.mid_position()
+        [hand_l, hand_r, foot_l, foot_r], target_positions, robot.mid_position()
     )
     ft = time.time()
-    print(f'complete ik in {(ft-bt)*1000:0.3f} msec')
-    print(f'ik result: \n{ik_result}')
+    print(f"complete ik in {(ft-bt)*1000:0.3f} msec")
+    print(f"ik result: \n{ik_result}")
     robot.fk(ik_result.x)
     # j_yaw.drive(angles[0])
     # j_pitch1.drive(angles[1])
@@ -62,58 +61,79 @@ def manipulator(filename: str,
 
     # print(f'manipulator structure: \n\r{robot.links[0].tree()}')
 
-    pytf.plot.corners(size=[1.1*size.max()]*3, center=0.5*(mi+ma), ax=ax)
+    pytf.plot.corners(size=[1.1 * size.max()] * 3, center=0.5 * (mi + ma), ax=ax)
 
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('z')
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("z")
 
-    pytf.plot.chain(robot, ax=ax, scale=size.mean()*0.1, show_label=False)
-    for i, t in enumerate(target_positions):
-        ax.scatter(
-            t[0], t[1], t[2],
-            s=80, label=f'target {i:02d}')
+    pytf.plot.chain(robot, ax=ax, scale=size.mean() * 0.1, show_label=False)
+    labels = [
+        "left_hand_default",
+        "right_hand_default",
+        "left_foot_default",
+        "right_foot_default",
+    ]
+    for t, l in zip(target_positions, labels):
+        ax.scatter(t[0], t[1], t[2], s=80, label=l)
     ax.legend()
     ax.view_init(elev=45, azim=45)
-    ax.set_proj_type('ortho')
-    ax.set_aspect('equal')
+    ax.set_proj_type("ortho")
+    ax.set_aspect("equal")
 
     if is_save:
-        image_name = 'ik_result_4limbs.png'
+        image_name = "ik_result_4limbs.png"
         plt.savefig(image_name)
-        print(f'write {image_name}')
+        print(f"write {image_name}")
     else:
         plt.show()
 
 
-def main():
+def ik_target_template(id: int):
+    template = [
+        # joy
+        [
+            np.array([0.25 + 1, 0.1, 1.0]),
+            np.array([0.25 + 1, -0.1, 0.2]),
+            np.array([0.0 + 1, 0.1, -0.6]),
+            np.array([0.1 + 1, -0.1, -0.4]),
+        ],
+        # floor
+        [
+            np.array([1 + 1, 0.1, 0.0]),
+            np.array([1 + 1, -0.1, 0.0]),
+            np.array([0.0 + 1, 0.1, -0.0]),
+            np.array([0.1 + 1, -0.1, -0.0]),
+        ],
+    ]
 
+    return template[id]
+
+
+def main():
     default_urdf = os.path.join(
-        os.path.dirname(__file__),
-        '../tests/files/human-xyz-165.urdf'
+        os.path.dirname(__file__), "../tests/files/human-xyz-165.urdf"
     )
 
     parser = ArgumentParser()
 
-    parser.add_argument('--urdf', default=default_urdf)
+    parser.add_argument("--urdf", default=default_urdf)
 
-    parser.add_argument('--target', type=float, nargs=3,
-                        help='target position for end effector',
-                        default=[1.5, 1.5, 4.0])
-    parser.add_argument('--method', default='BFGS')
-    parser.add_argument('--save', action='store_true')
+    parser.add_argument(
+        "--target",
+        type=int,
+        help="target position template id",
+        default=0,
+    )
+    parser.add_argument("--method", default="BFGS")
+    parser.add_argument("--save", action="store_true")
 
     args = parser.parse_args()
 
-    targets = [
-        np.array([0.25+1, 0.1, 1.0]),
-        np.array([0.25+1, -0.1, 0.2]),
-        np.array([0.0+1, 0.1, -0.6]),
-        np.array([0.1+1, -0.1, -0.4])
-    ]
-
-    manipulator(args.urdf, targets, args.save, method=args.method)
+    manipulator(
+        args.urdf, ik_target_template(args.target), args.save, method=args.method
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()

b8142960dfd6db887155196a33773904be9ec5bf
naoki_kubota <naoki_kubota@jtekt.co.jp>
2024-02-21 15:20:56 +0900

    Update examples

--- examples/ik.py
+++ examples/ik.py
@@ -17,62 +17,48 @@ from pytransform.tf import Transform
 def manipulator(target_position: np.ndarray, is_save: bool = False):
 
     # origin
-    base_link = Transform(
-        position=np.array([0.0, 0.0, 0.0]),
-        name='base_link')
+    base_link = Transform(position=np.array([0.0, 0.0, 0.0]), name="base_link")
 
     # define manipulator structure
-    arm_base = Transform(
-        position=np.array((0.0, 0.0, 0.0)),
-        name='arm_base'
-    )
+    arm_base = Transform(position=np.array((0.0, 0.0, 0.0)), name="arm_base")
 
-    link1 = Transform(
-        position=np.array((0.0, 0.0, 2.0)),
-        name='link1'
-    )
+    link1 = Transform(position=np.array((0.0, 0.0, 2.0)), name="link1")
 
-    link2 = Transform(
-        position=np.array((0.0, 0.0, 4.0)),
-        name='link2'
-    )
+    link2 = Transform(position=np.array((0.0, 0.0, 4.0)), name="link2")
 
-    link3 = Transform(
-        position=np.array((0.0, 0.0, 6.0)),
-        name='link3'
-    )
+    link3 = Transform(position=np.array((0.0, 0.0, 6.0)), name="link3")
 
-    end_effector = Transform(
-        position=np.array((0.0, 0.0, 8.0)),
-        name='end_effector'
-    )
+    end_effector = Transform(position=np.array((0.0, 0.0, 8.0)), name="end_effector")
 
     end_effector.set_parent(link3)
 
     j_yaw = RevoluteJoint(
-        parent=arm_base, child=link1,
-        origin=Transform(arm_base.position, name='yaw'),
+        parent=arm_base,
+        child=link1,
+        origin=Transform(arm_base.position, name="yaw"),
         axis=np.array([0, 0, 1]),
-        limit=Limitation(2, -2)
+        limit=Limitation(2, -2),
     )
 
     j_pitch1 = RevoluteJoint(
-        parent=link1, child=link2,
-        origin=Transform(link2.position, name='pitch1'),
+        parent=link1,
+        child=link2,
+        origin=Transform(link2.position, name="pitch1"),
         axis=np.array([0, 1, 0]),
-        limit=Limitation(3, -3)
+        limit=Limitation(3, -3),
     )
 
     j_pitch2 = RevoluteJoint(
-        parent=link2, child=link3,
-        origin=Transform(link3.position, name='pitch2'),
+        parent=link2,
+        child=link3,
+        origin=Transform(link3.position, name="pitch2"),
         axis=np.array([0, 1, 0]),
-        limit=Limitation(3, -3)
+        limit=Limitation(3, -3),
     )
 
     robot_arm = Chain(
         links=[arm_base, link1, link2, link3, end_effector],
-        joints=[j_yaw, j_pitch1, j_pitch2]
+        joints=[j_yaw, j_pitch1, j_pitch2],
     )
 
     # end of definition of the manipulator
@@ -81,40 +67,39 @@ def manipulator(target_position: np.ndarray, is_save: bool = False):
     bt = time.time()
     ik_result = robot_arm.ik_solve(
         [robot_arm.links[-1]],
-        [target_position]
+        [target_position],
+        initial_position=robot_arm.mid_position(),
     )
     ft = time.time()
-    print(f'complete ik in {(ft-bt)*1000:0.3f} msec')
-    print(f'ik result: \n{ik_result}')
+    print(f"complete ik in {(ft-bt)*1000:0.3f} msec")
+    print(f"ik result: \n{ik_result}")
     robot_arm.fk(ik_result.x)
     # j_yaw.drive(angles[0])
     # j_pitch1.drive(angles[1])
     # j_pitch2.drive(angles[2])
 
-    print(f'manipulator structure: \n\r{robot_arm.links[0].tree()}')
+    print(f"manipulator structure: \n\r{robot_arm.links[0].tree()}")
 
     # visualize
-    fig = plt.figure(figsize=(800/72, 800/72))
-    ax: plt.Axes = fig.add_subplot(projection='3d')
-    ax.set_aspect('equal')
+    fig = plt.figure(figsize=(800 / 72, 800 / 72))
+    ax: plt.Axes = fig.add_subplot(projection="3d")
+    ax.set_aspect("equal")
     pytf.plot.corners(size=(8, 8, 8), center=(0, 0, 4), ax=ax)
 
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('z')
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("z")
 
     pytf.plot.chain(robot_arm)
 
-    ax.scatter(
-        target_position[0], target_position[1], target_position[2],
-        s=80)
+    ax.scatter(target_position[0], target_position[1], target_position[2], s=80)
     ax.view_init(elev=45, azim=45)
-    ax.set_proj_type('ortho')
+    ax.set_proj_type("ortho")
 
     if is_save:
-        image_name = 'ik_result.png'
+        image_name = "ik_result.png"
         plt.savefig(image_name)
-        print(f'write {image_name}')
+        print(f"write {image_name}")
     else:
         plt.show()
 
@@ -122,15 +107,19 @@ def manipulator(target_position: np.ndarray, is_save: bool = False):
 def main():
     parser = ArgumentParser()
 
-    parser.add_argument('--target', type=float, nargs=3,
-                        help='target position for end effector',
-                        default=[1.5, 1.5, 4.0])
-    parser.add_argument('--save', action='store_true')
+    parser.add_argument(
+        "--target",
+        type=float,
+        nargs=3,
+        help="target position for end effector",
+        default=[1.5, 1.5, 4.0],
+    )
+    parser.add_argument("--save", action="store_true")
 
     args = parser.parse_args()
 
     manipulator(np.array(args.target), args.save)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- examples/ik_dual_arm.py
+++ examples/ik_dual_arm.py
@@ -6,37 +6,36 @@ from argparse import ArgumentParser
 
 import matplotlib.pyplot as plt
 import numpy as np
-import quaternion
 from mpl_toolkits.mplot3d.axes3d import Axes3D
 
 import pytransform as pytf
 from pytransform import urdf
-from pytransform.chain import Chain
-from pytransform.joint import Limitation, RevoluteJoint
 from pytransform.tf import Transform
 
 
-def manipulator(filename: str,
-                target_positions: list[np.ndarray],
-                is_save: bool = False):
+def manipulator(
+    filename: str, target_positions: list[np.ndarray], is_save: bool = False
+):
 
     robot = urdf.chain_from_urdf(filename)
-    print(f'robot name: {robot.name}')
-    print(f'#links: {len(robot.links)}')
-    print(f'#joints: {len(robot.joints)}')
+    print(f"robot name: {robot.name}")
+    print(f"#links: {len(robot.links)}")
+    print(f"#joints: {len(robot.joints)}")
+    hand_l = robot.get_link("left_hand_default")
+    if hand_l is None:
+        print("not found hand_l")
+        return
+    hand_r = robot.get_link("right_hand_default")
+    if hand_r is None:
+        print("not found hand_r")
+        return
 
-    hand_l = robot.get_link('l_wrist_default')
-    hand_r = robot.get_link('r_wrist_default')
-
+    target_tfs = [Transform(p) for p in target_positions]
     bt = time.time()
-    ik_result = robot.ik_solve(
-        [hand_l, hand_r],
-        target_positions,
-        robot.mid_position()
-    )
+    ik_result = robot.ik_solve([hand_l, hand_r], target_positions, robot.mid_position())
     ft = time.time()
-    print(f'complete ik in {(ft-bt)*1000:0.3f} msec')
-    print(f'ik result: \n{ik_result}')
+    print(f"complete ik in {(ft-bt)*1000:0.3f} msec")
+    print(f"ik result: \n{ik_result}")
     robot.fk(ik_result.x)
     # j_yaw.drive(angles[0])
     # j_pitch1.drive(angles[1])
@@ -45,31 +44,29 @@ def manipulator(filename: str,
     # print(f'manipulator structure: \n\r{robot.links[0].tree()}')
 
     # visualize
-    fig = plt.figure(figsize=(800/72, 800/72))
-    ax: plt.Axes = fig.add_subplot(projection='3d')
+    fig = plt.figure(figsize=(800 / 72, 800 / 72))
+    ax: plt.Axes = fig.add_subplot(projection="3d")
 
     mi, ma = robot.bbox()
-    size = (ma-mi)
-    pytf.plot.corners(size=1.1*size, center=0.5*(mi+ma), ax=ax)
+    size = ma - mi
+    pytf.plot.corners(size=1.1 * size, center=0.5 * (mi + ma), ax=ax)
 
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('z')
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("z")
 
-    pytf.plot.chain(robot, ax=ax, scale=size.mean()*0.1)
+    pytf.plot.chain(robot, ax=ax, scale=size.mean() * 0.1)
     for i, t in enumerate(target_positions):
-        ax.scatter(
-            t[0], t[1], t[2],
-            s=80, label=f'target {i:02d}')
+        ax.scatter(t[0], t[1], t[2], s=80, label=f"target {i:02d}")
     ax.legend()
     ax.view_init(elev=45, azim=45)
-    ax.set_proj_type('ortho')
-    ax.set_aspect('equal')
+    ax.set_proj_type("ortho")
+    ax.set_aspect("equal")
 
     if is_save:
-        image_name = 'ik_result.png'
+        image_name = "ik_result.png"
         plt.savefig(image_name)
-        print(f'write {image_name}')
+        print(f"write {image_name}")
     else:
         plt.show()
 
@@ -77,28 +74,28 @@ def manipulator(filename: str,
 def main():
 
     default_urdf = os.path.join(
-        os.path.dirname(__file__),
-        '../tests/files/human165.urdf'
+        os.path.dirname(__file__), "../tests/files/human165.urdf"
     )
 
     parser = ArgumentParser()
 
-    parser.add_argument('--urdf', default=default_urdf)
+    parser.add_argument("--urdf", default=default_urdf)
 
-    parser.add_argument('--target', type=float, nargs=3,
-                        help='target position for end effector',
-                        default=[1.5, 1.5, 4.0])
-    parser.add_argument('--save', action='store_true')
+    parser.add_argument(
+        "--target",
+        type=float,
+        nargs=3,
+        help="target position for end effector",
+        default=[1.5, 1.5, 4.0],
+    )
+    parser.add_argument("--save", action="store_true")
 
     args = parser.parse_args()
 
-    targets = [
-        np.array([0.25, 0.1, 0.8]),
-        np.array([0.25, -0.1, 0.8])
-    ]
+    targets = [np.array([0.25, 0.1, 0.8]), np.array([0.25, -0.1, 0.8])]
 
     manipulator(args.urdf, targets, args.save)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
